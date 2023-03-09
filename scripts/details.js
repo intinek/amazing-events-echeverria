bdfe4b9067a59e9232961c13d2e3973b82843e36
intinek <echeverriainti@hotmail.com>
2023-03-09 09:40:42 -0300

    Task-3

--- scripts/details.js
+++ scripts/details.js
@@ -1,4 +1,5 @@
-const datos = {
+let data =
+{
     "currentDate": "2022-09-01",
     "events": [
         {
@@ -447,35 +448,47 @@ const datos = {
         }
     ]
 }
+const queryString = location.search
+const params = new URLSearchParams(queryString)
+const id = params.get("id")
+const eventFound = data.events.find(event => event._id == id)
 
-let tarjetas = document.getElementById("tarjetas");
-
-function crearTarjetas(datos){
-    let tarj ="";
-    for (const event of datos.events){
-        tarj += `<div class="col">
-        <div class="card h-100">
-          <img
-            src="${event.image}"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">
-              ${event.description}
-            </p>
-            <p class="card-text"><small class="text-muted">${event.date}</small></p>
-          </div>
-          <div class="card-footer d-inline-flex justify-content-around">
-            <div class="d-flex align-items-center">
-              <span>$${event.price}</span>
+const div = document.querySelector("#contenedor")
+div.innerHTML =  `<div class="d-flex h-100 flex-md-row flex-column">
+<div class="col d-flex justify-content-center">
+    <img id="detail-img" src="${eventDetail.image}" class="img-fluid w-100 rounded-start" alt="...">
+</div>
+<div class="d-flex col align-items-center">
+    <div class="card-body">
+        <h5 class="card-title">${eventDetail.name}</h5>
+        <p class="card-text">${eventDetail.description}</p>
+        <div class="d-flex flex-wrap flex-column flex-sm-row border-top border-1 border-secondary">
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-person-square text-danger fs-1 pe-2"></i>
+                <span class="col-7">Capacity: ${eventDetail.capacity}</span>
             </div>
-            <a class="btn btn-dark" href="./details.html" role="button">Details</a>
-          </div>
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-currency-dollar text-danger fs-1 pe-2"></i>
+                <span class="col-7">Price: ${eventDetail.price}</span>
+            </div>
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-bookmark-fill text-danger fs-1 pe-2"></i>
+                <span class="col-7">Category: ${eventDetail.category}</span>
+            </div>
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-geo-alt-fill text-danger fs-1 pe-2"></i>
+                <span class="col-7">Place: ${eventDetail.place}</span>
+            </div>
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-calendar-event text-danger fs-1 pe-2"></i>
+                <span class="col-7">Date: ${eventDetail.date}</span>
+            </div>
+            <div class="d-flex text-center align-items-center justify-content-center card-text col-sm-6 col-12">
+                <i class="bi bi-person-fill-up text-danger fs-1 pe-2"></i>
+                <span class="col-7"> ${(eventDetail.assistance!=undefined) ? ("Assistance: "): ("Estimate: ")}  ${(eventDetail.assistance!=undefined) ? (eventDetail.assistance): (eventDetail.estimate)}</span>
+            </div>
+
         </div>
-      </div>
-        `;
-    }
-    return tarj;
-}
-tarjetas.innerHTML = crearTarjetas(datos);
+    </div>
+</div>
+</div>`
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const datos = {
+let Events =
+{
     "currentDate": "2022-09-01",
     "events": [
         {
@@ -448,36 +449,89 @@ const datos = {
     ]
 }
 
-let tarjetas = document.getElementById("tarjetas");
-let currentDate = datos.currentDate
+const contenedorTarjetas = document.getElementById('contenedor');
+const checkboxes = document.querySelectorAll('input[name="categoria"]');
 
-function crearTarjetas(datos){
-    let tarj ="";
-    for (const event of datos.events){
-        if(currentDate < event.date){
-        tarj += `<div class="col">
-        <div class="card h-100">
-          <img
-            src="${event.image}"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">${event.name}</h5>
-            <p class="card-text">
-              ${event.description}
-            </p>
-            <p class="card-text"><small class="text-muted">${event.date}</small></p>
-          </div>
-          <div class="card-footer d-inline-flex justify-content-around">
-            <div class="d-flex align-items-center">
-              <span>$${event.price}</span>
+function crearTarjetas(arrayData) {
+    let tarjetas = '';
+    for (const event of arrayData.events) {
+        tarjetas += `
+            <div class="col" id="event-${event._id}">
+                <div class="card h-100 shadow p-3 mb-5 bg-body-tertiary rounded">
+                    <img src="${event.image}" class="card-img-top" alt="card">
+                    <div class="card-body">
+                        <h4 class="card-title">${event.name}</h4>
+                        <p class="card-text">${event.description}</p>
+                        <h6 class ="card-category">Category: ${event.category}<h6>
+                        <h5 class="card-stock">In stock</h5>
+                    </div>
+                    <div class="card-footer d-inline-flex justify-content-around">
+                        <div class="d-flex align-items-center">
+                            <h5>Price: ${event.price}$</h5>
+                        </div>
+                        <a class="btn btn-dark" href="./html/details.html?id=${event._id}">Info</a>
+                    </div>
+                </div>
             </div>
-            <a class="btn btn-dark" href="./details.html" role="button">Details</a>
-          </div>
-        </div>
-      </div>
-        `;}
+        `;
     }
-    return tarj;
+
+    return tarjetas;
 }
-tarjetas.innerHTML = crearTarjetas(datos);
+
+contenedorTarjetas.innerHTML = crearTarjetas(Events);
+
+function filtrarPorCategoria(eventos, categorias) {
+    if (categorias.length === 0) {
+        return eventos;
+    } else {
+        const eventosFiltrados = eventos.filter((evento) => categorias.includes(evento.category));
+        return eventosFiltrados;
+    }
+}
+
+function actualizarTarjetas() {
+    const categoriasSeleccionadas = Array.from(checkboxes)
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.id);
+
+    const eventosFiltrados = filtrarPorCategoria(Events.events, categoriasSeleccionadas);
+
+    contenedorTarjetas.innerHTML = crearTarjetas({ events: eventosFiltrados });
+}
+
+function buscarEvento() {
+    const inputBuscador = document.getElementById('buscador');
+    const busqueda = inputBuscador.value.toLowerCase();
+    const tarjetas = document.getElementsByClassName('col');
+    let resultadosEncontrados = 0;
+    for (const tarjeta of tarjetas) {
+        const titulo = tarjeta.querySelector('.card-title').textContent.toLowerCase();
+        const descripcion = tarjeta.querySelector('.card-text').textContent.toLowerCase();
+        const categoria = tarjeta.querySelector('.card-category').textContent.toLowerCase();
+        if (titulo.includes(busqueda) || descripcion.includes(busqueda) || categoria.includes(busqueda)) {
+            tarjeta.style.display = 'block';
+            resultadosEncontrados++;
+        } else {
+            tarjeta.style.display = 'none';
+        }
+    }
+
+    if (resultadosEncontrados === 0) {
+        const contenedorTarjetas = document.getElementById('contenedor');
+        contenedorTarjetas.innerHTML = 
+        `
+        <div class="home w-100">
+        <p class="mt-4 fs-1 fw-bold">Not Found</p>
+        </div>
+        
+        `;
+    }
+}
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', actualizarTarjetas);
+});
+
+btnBuscar.addEventListener('click', buscarEvento);
+
